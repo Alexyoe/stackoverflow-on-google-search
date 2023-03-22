d983dabb4b124a1282a6ff3e3c42fe97d7cbdb42
Alex Yoesting <alexyoe@icloud.com>
2023-03-22 18:49:09 -0400

    2.0.0 Release

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
-# Stack Overflow Search On Google
-A simple userscript that adds a button to your Google searches to only show results from stackoverflow.com . You can just click on the button and it will automatically add `site:stackoverflow.com` to your search.
+# Stack Overflow On Google Search
+A simple userscript that adds a button to your Google searches to only show results from stackoverflow.com. You can just click on the button and it will automatically add `site:stackoverflow.com` to your search!
 
 ![Img](screenshot.png)
 
 ## Downloading the script
 The direct download link is here:
 
-* [GreasyFork](https://greasyfork.org/en/scripts/381497-reddit-search-on-google)
+* [GreasyFork](https://greasyfork.org/en/scripts/453125-stack-overflow-search-on-google)
 
 **Chrome users:** You will need a script manager extension like [TamperMonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)  or [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/). Then go to the download link above and click on 'Install X.X'. Finally, click on 'install' in the new window that will open.
 
 **Firefox users:** you'll need a script manager extension like [TamperMonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en). Then go to the download link above and click on 'Install X.X'. Finally, click on 'install' in the new window that will open.
 
 **Other browsers:** you can look for a script manager extension in your browser add-ons page. Look for Tampermonkey or Greasemonkey and install them. Then go to the download link above and click on 'Install X.X'. Finally, click on 'install' in the new window that will open.
-
-## Customizing the script
-There are a few options you can change inside the script:
-
-* `useIcon` can be set to `false` if you don't want to see an icon inside the Stack Overflow button. By default it's set to `true`
-* `appendRight` can be set to `true` if you want to append the Stack Overflow button to the right of Google's buttons (next to 'Tools'). By default it's set to `false`, which appends it next to the usual buttons like 'Videos'.
 
 ## Credit
 This is a fork of marioortizmanero's [Reddit-search-on-google](https://github.com/marioortizmanero/reddit-search-on-google)
--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Stack Overflow search on Google
-// @version      1.0.0
-// @description  Adds a button to search stackoverflow.com with Google
+// @name         Stack Overflow on Google Search
+// @version      2.0.0
+// @description  Adds a button to search stackoverflow via Google Search
 // @author       Alexyoe
 // @namespace    https://github.com/Alexyoe/stackoverflow-search-on-google.git
 // @include      http*://www.google.*/search*
@@ -10,60 +10,57 @@
 // @license MIT
 // ==/UserScript==
 
-// Change this to false if you don't want an icon
-const useIcon = true;
-// Change this to true if you want to add the button to the right of the 'Tools' button
-const appendRight = false;
-
 const queryRegex = /q=[^&]+/g;
 const siteRegex = /\+site(?:%3A|\:).+\.[^&+]+/g;
-const stackoverflowUrl = '+site%3Astackoverflow.com';
-let stackoverflowIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 384 512"><path d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z"/></svg>';
+const stackoverflowUrl = "+site%3Astackoverflow.com";
+let stackoverflowIcon =
+  '<svg class="DCxYpf" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 384 512"><path d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z"/></svg>';
 const isImageSearch = /[?&]tbm=isch/.test(location.search);
+console.log(isImageSearch);
 
-if (typeof trustedTypes !== 'undefined') {
-    const policy = trustedTypes.createPolicy('html', { createHTML: input => input });
-    stackoverflowIcon = policy.createHTML(stackoverflowIcon);
+if (typeof trustedTypes !== "undefined") {
+  const policy = trustedTypes.createPolicy("html", {
+    createHTML: (input) => input,
+  });
+  stackoverflowIcon = policy.createHTML(stackoverflowIcon);
 }
 
 (function () {
-    // Creating the element
-    let el = document.createElement('div');
-    el.className = 'hdtb-mitem';
-    const link = document.createElement('a');
+  // Create the link element
+  const el = document.createElement("a");
+  el.className = isImageSearch ? "NZmxZe" : "zItAnd FOU1zf GMT2kb";
 
-    // Adding the svg icon
-    if (useIcon) {
-        const span = document.createElement('span');
-        span.className = isImageSearch ? 'm3kSL' : 'bmaJhd iJddsb';
-        span.style.cssText = 'height:16px;width:16px';
-        span.innerHTML = stackoverflowIcon;
-        link.appendChild(span);
-    }
+  // Add icon to the link
+  const span = document.createElement("span");
+  span.className = isImageSearch ? "m3kSL" : "mUKzod";
+  span.style.cssText = "height:16px;width:16px";
+  span.innerHTML = stackoverflowIcon;
+  el.appendChild(span);
 
-    // Hyperlink to add 'site:stackoverflow.com' to the query
-    link.appendChild(document.createTextNode('Stack Overflow'));
-    link.href = window.location.href.replace(queryRegex, (match) => {
-        // Replaces the existing `site` flags
-        return match.search(siteRegex) >= 0 ? match.replace(siteRegex, stackoverflowUrl) : match + stackoverflowUrl;
-    });
-    if (isImageSearch) {
-        link.classList.add('NZmxZe');
-        el = link;
-    } else {
-        el.appendChild(link);
-    }
+  // Create the div element for the text
+  const link = document.createElement("div");
+  link.textContent = "Stack Overflow";
+  el.appendChild(link);
 
-    // Inserting the element into Google search
-    if (appendRight) {
-        const toolsBtn = document.querySelector(isImageSearch ? '.ssfWCe' : '.t2vtad');
-        toolsBtn.parentNode.insertBefore(el, toolsBtn.nextSibling);
-    } else {
-        const menuBar = document.querySelector(isImageSearch ? '.T47uwc' : '.MUFPAc');
-        if (isImageSearch) {
-            menuBar.insertBefore(el, menuBar.children[menuBar.childElementCount - 1]);
-        } else {
-            menuBar.appendChild(el);
-        }
-    }
+  // Add site:stackoverflow.com to the query
+  el.href = window.location.href.replace(queryRegex, (match) =>
+    match.search(siteRegex) >= 0
+      ? match.replace(siteRegex, stackoverflowUrl)
+      : match + stackoverflowUrl
+  );
+
+  // Insert the link into Google search
+  const menuBar = document.querySelector(isImageSearch ? ".T47uwc" : ".nfdoRb");
+  if (isImageSearch) {
+    menuBar.insertBefore(el, menuBar.children[menuBar.childElementCount - 1]);
+  } else {
+    menuBar.appendChild(el);
+  }
+
+  // Fix Sizing
+  const buttonBox = document.querySelector(".TrmO7");
+  buttonBox.classList.add("size-fix");
+  const buttonBoxCSS = document.createElement("style");
+  buttonBoxCSS.innerHTML = ".size-fix { width: 100% !important; }";
+  document.head.appendChild(buttonBoxCSS);
 })();
